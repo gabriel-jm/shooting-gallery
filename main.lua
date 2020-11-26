210af5b37102a936fa41ceca2444b74ed69c7413
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-26 14:25:47 -0300

    Making a simple click event on the game

---
+++ main.lua
@@ -0,0 +1,37 @@
+function love.load()
+  gameFont = love.graphics.newFont(40)
+
+  target = {
+    x = 150,
+    y = 150,
+    radius = 50
+  }
+
+  score = 0
+  timer = 0
+end
+
+function love.update(deltaTime)
+end
+
+function love.draw()
+  love.graphics.setFont(gameFont)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print('Score: '..score, 0, 0)
+
+  love.graphics.setColor(0.8, 0.2, 0.2)
+  love.graphics.circle('fill', target.x, target.y, target.radius)
+end
+
+function love.mousepressed(x, y, button, isTouch, presses)
+  actions = {
+    function ()
+      score = score + 1
+    end,
+    function ()
+      score = score - 1
+    end
+  }
+
+  if actions[button] ~= nil then actions[button]() end
+end
